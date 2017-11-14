a646206fdac823291d4a32be66848c446de03bc1
thatdania@gmail.com <thatdania@gmail.com>
2017-11-14 12:17:08 +0000

    Added the page response check

--- spec/features/viewing_links_spec.rb
+++ spec/features/viewing_links_spec.rb
@@ -4,6 +4,9 @@ feature 'viewing list of links on homepage' do
   scenario 'user navigates to homepage' do
     Link.create(name: 'Daniablog', url: 'https://hackernoon.com/@thatdania')
     visit('/links')
+    expect(page.status_code).to eq 200 
     expect(page).to have_content 'Daniablog'
   end
+
+
 end
