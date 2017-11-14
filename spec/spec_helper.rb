21704d28eb54e3067fc3031f247cf754c58c61cf
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-11-14 11:49:46 +0000

    links capybara to app class

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
+require File.join(File.dirname(File.absolute_path(__FILE__)),
+                  '..', 'app', 'app.rb')
 
 ENV['RACK_ENV'] = 'test'
+Capybara.app = App
