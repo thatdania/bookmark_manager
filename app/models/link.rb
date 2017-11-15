85f3eb998487c0de11f13903df6e941c38b4fda2
Andrew Davey <ajdavey8@gmail.com>
2017-11-15 13:42:45 +0000

    TYPO

--- app/models/link.rb
+++ app/models/link.rb
@@ -12,7 +12,8 @@ end
 # DataMapper::Logger.new(
 #   File.dirname(File.absolute_path(__FILE__)) + '/log.txt', :debug)
 
-DataMapper::setup(:default, ENV['DATABASE URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+
+DataMapper::setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
 
